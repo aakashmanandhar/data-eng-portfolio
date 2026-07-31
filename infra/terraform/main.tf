803957f3f747a602eb9eda0f5efb06065cf20a60
root <root@VM-4dd7629a-29db-4c29-aae3-4d99e1e1f32b.cs1local>
2026-07-31 10:30:26 +0000

    Update react container to production build image, add root user to airflow container for docker socket access

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -137,13 +137,14 @@ resource "docker_container" "django" {
   depends_on = [docker_container.postgres]
 }
 
-resource "docker_image" "react_dev" {
-  name = "node:20-slim"
+resource "docker_image" "react" {
+  name         = "portfolio-react:latest"
+  keep_locally = true
 }
 
 resource "docker_container" "react" {
   name  = "portfolio_react"
-  image = docker_image.react_dev.image_id
+  image = docker_image.react.image_id
 
   networks_advanced {
     name = docker_network.portfolio_net.name
@@ -154,13 +155,6 @@ resource "docker_container" "react" {
     external = 5173
   }
 
-  volumes {
-    host_path      = "${abspath(path.module)}/../../apps/web"
-    container_path = "/app"
-  }
-
-  working_dir = "/app"
-  command     = ["sh", "-c", "npm install && npm run dev -- --host 0.0.0.0"]
 }
 
 resource "docker_image" "dbt" {
@@ -197,6 +191,7 @@ resource "docker_image" "airflow" {
 resource "docker_container" "airflow" {
   name  = "portfolio_airflow"
   image = docker_image.airflow.image_id
+  user  = "root"
   networks_advanced {
     name = docker_network.portfolio_net.name
   }
